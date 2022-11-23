251d1f4203e3b2e429c09fef9636ba378de83f05
PKrystian <krystian.pinczak2001@gmail.com>
2022-11-23 19:33:11 +0100

    test2

--- Test1/test1.java
+++ Test1/test1.java
@@ -1,6 +1,6 @@
 package Test1;
 class test1{  
     public static void main(String args[]){  
-        System.out.println("Hello Java");   
+        System.out.println("Hello Java test1");   
     }  
 }  
