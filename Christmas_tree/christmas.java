5317b9841f2b8cc0d46de4f0352a19fe9187803e
PKrystian <krystian.pinczak2001@gmail.com>
2022-11-23 20:14:22 +0100

    Christmas tree, Height from input

--- Christmas_tree/christmas.java
+++ Christmas_tree/christmas.java
@@ -1,7 +1,22 @@
 package Christmas_tree;
+import java.util.Scanner;
 
 public class christmas {
     public static void main(String args[]){  
         System.out.println("Christmas Tree");
+        try (Scanner keyboard = new Scanner (System.in)) {
+            int height;
+            System.out.print("Enter a number: ");
+            height=keyboard.nextInt();
+            for(int i=1; i<=height; i++){
+                for(int gap=1; gap<=height-i;gap++){
+                    System.out.print(" ");
+                }
+                for(int star=1; star<=i*2-1;star++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
     }  
 }
